f79620add32a028e2f91a8bbc51a7ecd21f68192
lixvda <1547446385@qq.com>
2023-01-31 10:07:56 +0800

    finish: 2023-1-31-ppig-easy

---
+++ go/2023-1-31-ppig-easy.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+如果一个正方形矩阵满足下述 全部 条件，则称之为一个 X 矩阵 ：
+
+矩阵对角线上的所有元素都 不是 0
+矩阵中所有其他元素都是 0
+给你一个大小为 n x n 的二维整数数组 grid ，表示一个正方形矩阵。如果 grid 是一个 X 矩阵 ，返回 true ；否则，返回 false 。
+
+See https://leetcode.cn/problems/check-if-matrix-is-x-matrix/ for more details
+*/
+
+func checkXMatrix(grid [][]int) bool {
+	for i, row := range grid {
+		for j, v := range row {
+			if i == j || i+j == len(grid)-1 {
+				if v == 0 {
+					return false
+				}
+			} else if v != 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
