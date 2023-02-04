98a35f8a5ce3ff68dce17fe3e278687f2075148d
Pinkpig <59948049+Ignorant-lxf@users.noreply.github.com>
2023-02-04 16:07:26 +0800

    finish: 2023-1-30-ppig-mid (#15)

---
+++ go/2023-1-30-ppig-mid.go
@@ -0,0 +1,44 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// See https://leetcode.cn/problems/merge-in-between-linked-lists/description/ for more details
+func mergeInBetween(list1 *ListNode, a int, b int, list2 *ListNode) *ListNode {
+	var (
+		head = list1
+		idx  = 0
+		pre  *ListNode
+		next *ListNode
+	)
+
+	for list1 != nil {
+		if idx == a-1 {
+			pre = list1
+		}
+		if idx == b+1 {
+			next = list1
+			break
+		}
+		idx++
+		list1 = list1.Next
+	}
+
+	pre.Next = list2
+	for list2.Next != nil {
+		list2 = list2.Next
+	}
+	list2.Next = next
+
+	return head
+}
+
+/*
+	简单模拟即可
+*/
