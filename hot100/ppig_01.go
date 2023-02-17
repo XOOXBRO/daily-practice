101bde049e40c39641828ebcfa0d3919235c6fe9
lixvda <1547446385@qq.com>
2023-02-17 10:44:50 +0800

    ppig:hot 1 and 2

---
+++ hot100/ppig_01.go
@@ -0,0 +1,23 @@
+package hot100
+
+/*
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+
+See https://leetcode.cn/problems/two-sum/?favorite=2cktkvj for more details
+*/
+
+func twoSum(nums []int, target int) []int {
+	rec := make(map[int]int, len(nums))
+	for i, v := range nums {
+		idx, ok := rec[target-v]
+		if ok {
+			return []int{i, idx}
+		}
+		rec[v] = i
+	}
+	return []int{}
+}
